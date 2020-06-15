055411e3b1cc886941fc23237e37c1691906df6a
bert2 <shuairan@gmail.com>
2020-06-15 12:15:22 +0200

    implement

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ use std::fs;
 fn main() {
     println!("Your code goes here!");
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+    let args: Vec<String> = env::args().collect();
+    if args[1] == "init" {
+        fs::create_dir(".git")
+            .and_then(|()| fs::create_dir(".git/objects"))
+            .and_then(|()| fs::create_dir(".git/refs"))
+            .and_then(|()| fs::write(".git/HEAD", "ref: refs/heads/master\n"))
+            .unwrap();
+        println!("Initialized git directory")
+    } else {
+        println!("unknown command: {}", args[1])
+    }
 }
