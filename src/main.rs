02139a3ef0dfa0de44ad71b7cc69b5e8561e10b2
bert2 <shuairan@gmail.com>
2020-06-19 00:11:33 +0200

    fix compiler errors

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ use std::io::prelude::*;
 type Res<T> = std::result::Result<T, Box<dyn Error>>;
 
 fn main() {
-    fn run(args: &Skip<Args>) -> Res<String> {
+    fn run(args: &mut Skip<Args>) -> Res<String> {
         let cmd = args.next()
             .ok_or("no command provided. available commands: init, cat-file.")?;
         match cmd.as_str() {
             "init" => init(),
-            "cat-file" => cat_file(&args),
-            s => Err(format!("unknown command '{}'.", s))
+            "cat-file" => cat_file(args),
+            _ => Err(format!("unknown command '{}'.", cmd).into())
+            // s => Err(["unknown command '", cmd, "'."].concat().into())
         }
     }
 
-    let args = env::args().skip(1);
-    let exit_code = match run(&args) {
+    let mut args = env::args().skip(1);
+    let exit_code = match run(&mut args) {
         Ok(msg) => {
             println!("{}", msg);
             0
@@ -41,8 +42,8 @@ fn init() -> Res<String> {
     Ok("initialized git directory.".to_string())
 }
 
-fn cat_file(args: &Skip<Args>) -> Res<String> {
-    fn assert_prettyprint(args: &Skip<Args>) -> Res<()> {
+fn cat_file(args: &mut Skip<Args>) -> Res<String> {
+    fn assert_prettyprint(args: &mut Skip<Args>) -> Res<()> {
         let arg = args.next()
             .ok_or("not enough arguments provided for command cat-file. missing flag '-p'.")?;
         match arg.as_str() {
@@ -51,7 +52,7 @@ fn cat_file(args: &Skip<Args>) -> Res<String> {
         }
     }
 
-    fn validate_sha(args: &Skip<Args>) -> Res<String> {
+    fn validate_sha(args: &mut Skip<Args>) -> Res<String> {
         let sha = args.next()
             .ok_or("not enough arguments provided for command cat-file. missing SHA.")?;
         if sha.len() < 3 {
@@ -67,9 +68,9 @@ fn cat_file(args: &Skip<Args>) -> Res<String> {
     let (dir, filename) = sha.split_at(3);
     let file = fs::File::open([dir, "/", filename].concat())?;
 
-    let dec = GzDecoder::new(file);
+    let mut decoder = GzDecoder::new(file);
     let mut contents = String::new();
-    dec.read_to_string(&mut contents)?;
+    decoder.read_to_string(&mut contents)?;
 
     Ok(contents)
 }
