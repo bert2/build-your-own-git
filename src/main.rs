7d40f1803e0c561ab29a92c13853d79bf8e2d768
bert2 <shuairan@gmail.com>
2020-06-18 21:52:33 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
+use std::fmt;
 use std::fs;
+use std::io;
 
 fn main() {
-    println!("Your code goes here!");
-
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git")
-            .and_then(|()| fs::create_dir(".git/objects"))
-            .and_then(|()| fs::create_dir(".git/refs"))
-            .and_then(|()| fs::write(".git/HEAD", "ref: refs/heads/master\n"))
-            .unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    let exit_code = match run(&args) {
+        Ok(msg) => {
+            println!("{}", msg);
+            0
+        },
+        Err(err) => {
+            println!("error: {}", err);
+            1
+        }
+    };
+    std::process::exit(exit_code);
+}
+
+fn run(args: &Vec<String>) -> Result<String, String> {
+    match args[1].as_str() {
+        "init" => init()
+            .map(|()| "initialized git directory.".to_string())
+            .map_err(stringify),
+        s => Err(format!("unknown command '{}'.", s))
     }
+}
+
+fn init() -> io::Result<()> {
+    fs::create_dir(".git")?;
+    fs::create_dir(".git/objects")?;
+    fs::create_dir(".git/refs")?;
+    fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+    Ok(())
+}
+
+fn stringify<T: fmt::Display>(x: T) -> String {
+    format!("{}", x)
 }
