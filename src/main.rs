a395f7b69be111457eec321486e8fa0f68c2cd0b
bert2 <shuairan@gmail.com>
2020-06-20 16:35:37 +0200

    clean up

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,29 @@ type Res<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() {
     fn run(args: &mut Args) -> Res<String> {
-        let cmd = args.next()
-            .ok_or("no command provided. available commands: init, cat-file.")?;
+        let cmd = args.next().ok_or("No command provided.")?;
         match cmd.as_str() {
             "init"        => init(),
             "cat-file"    => cat_file(args),
             "hash-object" => hash_object(args),
-            _ => Err(format!("unknown command '{}'.", cmd).into())
+            _ => Err(format!("Unknown command '{}'.", cmd).into())
         }
     }
 
     let mut args = env::args();
-    args.next();
+    args.next(); // skip name of executable
+
     let exit_code = match run(&mut args) {
         Ok(msg) => {
             print!("{}", msg);
             0
         },
         Err(err) => {
-            println!("error: {}", err);
+            println!("ERROR: {}", err);
             1
         }
     };
+
     std::process::exit(exit_code);
 }
 
@@ -36,96 +37,99 @@ fn init() -> Res<String> {
     fs::create_dir(".git/objects")?;
     fs::create_dir(".git/refs")?;
     fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
-    Ok("initialized git directory.".to_string())
+    Ok("Initialized git directory.".to_string())
 }
 
 fn cat_file(args: &mut Args) -> Res<String> {
-    fn assert_prettyprint(args: &mut Args) -> Res<()> {
-        let arg = args.next()
-            .ok_or("not enough arguments provided for command cat-file. missing flag '-p'.")?;
-        match arg.as_str() {
-            "-p" => Ok(()),
-            _ => Err(format!("cat-file expects flag '-p'. got '{}' instead.", arg))?
+    fn validate_sha(args: &mut Args) -> Res<String> {
+        let sha = args.next().ok_or("Missing SHA argument.")?;
+        match sha.len() {
+            40 => Ok(sha),
+            _  => Err("Provided SHA does not have the required length of 40 characters.".into())
         }
     }
 
-    fn validate_sha(args: &mut Args) -> Res<String> {
-        let sha = args.next()
-            .ok_or("not enough arguments provided for command cat-file. missing SHA.")?;
+    fn open_object(sha: &str) -> Res<fs::File> {
+        let (dir, filename) = sha.split_at(2);
+        let path = ["./.git/objects/", dir, "/", filename].concat();
+        let file = fs::File::open(&path)
+            .map_err(|e| format!("Failed to read object '{}'. {}", path, e))?;
+        Ok(file)
+    }
+
+    fn decompress(file: fs::File) -> Res<String> {
+        let mut decoder = ZlibDecoder::new(file);
+        let mut decompressed = String::new();
+        decoder.read_to_string(&mut decompressed)?;
+        Ok(decompressed)
+    }
+
+    fn parse_content<'a>(content: &'a String) -> Res<(&'a str, &'a str)> {
+        let mut split = content.split('\x00');
+        let header = split.next().unwrap(); // `split` cannot be empty
+        let data = split.next().ok_or("Object content could not be parsed.")?;
+        Ok((header, data))
+    }
+
+    expect_flag(args, 'p')?;
+    let sha = validate_sha(args)?;
+    let file = open_object(&sha)?;
+    let decompressed = decompress(file)?;
+    let (_, data) = parse_content(&decompressed)?;
+    Ok(data.to_string())
+}
+
+fn hash_object(args: &mut Args) -> Res<String> {
+    fn create_content_from_file(file: &str) -> Res<String> {
+        let mut data = String::new();
+        fs::File::open(file)?.read_to_string(&mut data)
+            .map_err(|e| format!("Failed to read file '{}'. {}", file, e))?;
+        let header = format!("blob {}", &data.len());
+        let content = format!("{}\x00{}", header, data);
+        Ok(content)
+    }
+
+    fn compute_sha(content: &str) -> Res<String> {
+        let sha =  Sha1::digest(content.as_bytes()).iter()
+            .map(|byte| format!("{:02x}", byte))
+            .fold(String::new(), |sha, hex| sha + &hex);
         if sha.len() != 40 {
-            Err("provided SHA does not have the required length of 40 characters.")?
+            Err(format!("Generated SHA '{}' is invalid.", sha).into())
         } else {
             Ok(sha)
         }
     }
 
-    fn parse_content(content: String) -> Res<(String, String)> {
-        let mut split = content.split('\x00');
-        let header = split.next().ok_or("object content could not be parsed.")?;
-        let data = split.next().ok_or("object content could not be parsed.")?;
-        Ok((header.to_string(), data.to_string()))
+    fn create_object(sha: &str) -> Res<fs::File> {
+        let (dir, filename) = sha.split_at(2);
+        fs::create_dir_all(["./.git/objects/", dir].concat())?;
+        let path = ["./.git/objects/", dir, "/", filename].concat();
+        let file = fs::File::create(&path)
+            .map_err(|e| format!("Failed to create object '{}'. {}", path, e))?;
+        Ok(file)
     }
 
-    assert_prettyprint(args)?;
-    let sha = validate_sha(args)?;
+    fn write_object(object: fs::File, content: &str) -> Res<()> {
+        let mut encoder = ZlibEncoder::new(object, Compression::default());
+        encoder.write(content.as_bytes())?;
+        Ok(())
+    }
 
-    let (dir, filename) = sha.split_at(2);
-    let path = ["./.git/objects/", dir, "/", filename].concat();
-    let file = fs::File::open(&path)
-        .map_err(|e| format!("failed to read object '{}'. {}", path, e))?;
-
-    let mut decoder = ZlibDecoder::new(file);
-    let mut decompressed = String::new();
-    decoder.read_to_string(&mut decompressed)?;
-
-    let (_, data) = parse_content(decompressed)?;
-
-    Ok(data)
+    expect_flag(args, 'w')?;
+    let in_file = args.next().ok_or("Missing file argument.")?;
+    let content = create_content_from_file(&in_file)?;
+    let sha = compute_sha(&content)?;
+    let out_file = create_object(&sha)?;
+    write_object(out_file, &content)?;
+    Ok(sha)
 }
 
-fn hash_object(args: &mut Args) -> Res<String> {
-    fn assert_write(args: &mut Args) -> Res<()> {
-        let arg = args.next()
-            .ok_or("not enough arguments provided for command hash-object. missing flag '-w'.")?;
-        match arg.as_str() {
-            "-w" => Ok(()),
-            _ => Err(format!("hash-object expects flag '-w'. got '{}' instead.", arg))?
-        }
+fn expect_flag(args: &mut Args, flag: char) -> Res<()> {
+    let arg = args.next()
+        .ok_or(format!("Not enough arguments provided: missing flag '-{}'.", flag))?;
+    if arg == format!("-{}", flag) {
+        Ok(())
+    } else {
+        Err(format!("Expecting flag '-{}'. Got '{}' instead.", flag, arg).into())
     }
-
-    fn get_filename(args: &mut Args) -> Res<String> {
-        let name = args.next()
-            .ok_or("not enough arguments provided for command hash-object. missing file path.")?;
-        Ok(name)
-    }
-
-    fn create_content(filename: &String) -> Res<String> {
-        let mut in_data = String::new();
-        fs::File::open(filename)?.read_to_string(&mut in_data)
-            .map_err(|e| format!("failed to read file '{}'. {}", filename, e))?;
-        let content = ["blob ", &in_data.len().to_string(), "\x00", &in_data].concat();
-        Ok(content)
-    }
-
-    fn compute_sha1(content: &String) -> Res<String> {
-        let sha_bytes = Sha1::digest(content.as_bytes());
-        let sha: Vec<String> = sha_bytes.iter().map(|x| format!("{:02x}", x)).collect();
-        Ok(sha.concat())
-    }
-
-    assert_write(args)?;
-    let in_filename = get_filename(args)?;
-
-    let in_content = create_content(&in_filename)?;
-    let sha = compute_sha1(&in_content)?;
-    let (dir, out_filename) = sha.split_at(2);
-
-    fs::create_dir_all(["./.git/objects/", dir].concat())?;
-    let out_filepath = ["./.git/objects/", dir, "/", out_filename].concat();
-    let out_file = fs::File::create(&out_filepath)
-        .map_err(|e| format!("failed to create blob '{}'. {}", out_filepath, e))?;
-    let mut encoder = ZlibEncoder::new(out_file, Compression::default());
-    encoder.write(in_content.as_bytes())?;
-
-    Ok(sha)
 }
