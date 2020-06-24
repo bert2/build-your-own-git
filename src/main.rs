701df19c492b80f1808dceb7faf8c341d6c16171
bert2 <shuairan@gmail.com>
2020-06-24 21:36:44 +0200

    implement ls-remote

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use std::{env, env::Args, fs, fs::DirEntry, io::prelude::*, iter, iter::Peekable, path::Path, str, time::{SystemTime, UNIX_EPOCH}};
+use std::{
+    cmp::max,
+    env, env::Args,
+    fs, fs::DirEntry,
+    io::prelude::{Read, Write},
+    iter, iter::Peekable,
+    path::Path,
+    str,
+    time::{SystemTime, UNIX_EPOCH}
+};
+use bytes::Bytes;
 use flate2::{read::ZlibDecoder, write::ZlibEncoder, Compression};
+use reqwest::blocking::Client;
 use sha1::{Sha1, Digest};
 
-type Res<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+type R<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() {
-    fn run(args: &mut Peekable<Args>) -> Res<String> {
+    fn run(args: &mut Peekable<Args>) -> R<String> {
         let cmd = args.next().ok_or("No command provided.")?;
         match cmd.as_str() {
             "cat-file"    => cat_file(args),
             "commit-tree" => commit_tree(args),
             "hash-object" => hash_object(args),
             "init"        => init(),
+            "ls-remote"   => ls_remote(args),
             "ls-tree"     => ls_tree(args),
             "write-tree"  => write_tree(),
             _ => Err(format!("Unknown command '{}'.", cmd).into())
@@ -35,8 +47,10 @@ fn main() {
     std::process::exit(exit_code);
 }
 
-fn cat_file(args: &mut Peekable<Args>) -> Res<String> {
-    fn parse_blob_content<'a>(content: &'a String) -> Res<(&'a str, &'a str)> {
+// commands
+
+fn cat_file(args: &mut Peekable<Args>) -> R<String> {
+    fn parse_blob_content<'a>(content: &'a String) -> R<(&'a str, &'a str)> {
         let mut split = content.split('\x00');
         let header = split.next().unwrap();
         let data = split.next().ok_or("Content of blob object could not be parsed.")?;
@@ -52,7 +66,7 @@ fn cat_file(args: &mut Peekable<Args>) -> Res<String> {
     Ok(data.to_string())
 }
 
-fn commit_tree(args: &mut Peekable<Args>) -> Res<String> {
+fn commit_tree(args: &mut Peekable<Args>) -> R<String> {
     let tree = parse_arg(args, "SHA")?;
     validate_sha(&tree)?;
     let parent = parse_opt_arg_named(args, "-p")?;
@@ -70,7 +84,7 @@ fn commit_tree(args: &mut Peekable<Args>) -> Res<String> {
     Ok(sha)
 }
 
-fn hash_object(args: &mut Peekable<Args>) -> Res<String> {
+fn hash_object(args: &mut Peekable<Args>) -> R<String> {
     parse_arg_flag(args, "-w")?;
     let in_file = parse_arg(args, "file")?;
     let content = create_blob_from_file(Path::new(&in_file))?;
@@ -80,7 +94,7 @@ fn hash_object(args: &mut Peekable<Args>) -> Res<String> {
     Ok(sha)
 }
 
-fn init() -> Res<String> {
+fn init() -> R<String> {
     fs::create_dir(".git")?;
     fs::create_dir(".git/objects")?;
     fs::create_dir(".git/refs")?;
@@ -88,8 +102,8 @@ fn init() -> Res<String> {
     Ok("Initialized git directory.".to_string())
 }
 
-fn ls_tree(args: &mut Peekable<Args>) -> Res<String> {
-    fn parse_tree_content(content: &Vec<u8>) -> Res<Vec<&str>> {
+fn ls_tree(args: &mut Peekable<Args>) -> R<String> {
+    fn parse_tree_content(content: &Vec<u8>) -> R<Vec<&str>> {
         fn iterate_tree(bytes: &Vec<u8>) -> impl Iterator<Item = &[u8]> {
             let mut bytes = bytes.as_slice();
             let mut sha_len = 0; // header has no SHA
@@ -103,7 +117,7 @@ fn ls_tree(args: &mut Peekable<Args>) -> Res<String> {
             })
         }
 
-        fn get_name(entry: &str) -> Res<&str> {
+        fn get_name(entry: &str) -> R<&str> {
             entry.split(' ')
                 .skip(1) // skip mode
                 .next()  // get name
@@ -127,9 +141,57 @@ fn ls_tree(args: &mut Peekable<Args>) -> Res<String> {
     Ok(data.join("\n") + "\n")
 }
 
-fn write_tree() -> Res<String> {
-    fn render_entry(entry: &DirEntry) -> Res<Vec<u8>> {
-        fn render_file(entry: &DirEntry) -> Res<Vec<u8>> {
+fn ls_remote(args: &mut Peekable<Args>) -> R<String> {
+    fn pkt_lines(bytes: Bytes) -> impl Iterator<Item = String> {
+        let mut bytes = bytes;
+        iter::from_fn(move || {
+            if bytes.len() == 0 { return None; }
+            let len = str::from_utf8(&bytes[..4]).expect("Failed to read pkt-len.");
+            let len = usize::from_str_radix(len, 16).expect("Failed to parse pkt-len.");
+            let pkt_line = match len {
+                0 => String::new(),
+                _ => str::from_utf8(&bytes[4..len]).unwrap().to_string()
+            };
+
+            bytes = bytes.slice(max(len, 4)..);
+
+            Some(pkt_line)
+        })
+    }
+
+    fn parse_ref(pkt_line: String) -> R<(String, String)> {
+        let _ref = pkt_line.split('\0').next().unwrap();
+        let mut ref_parts = _ref.split(' ');
+        let id = ref_parts.next().unwrap();
+        let name = ref_parts.next().ok_or("Failed to parse ref pkt-line.")?.trim();
+        Ok((id.to_string(), name.to_string()))
+    }
+
+    fn print_ref(_ref: R<(String, String)>) -> R<String> {
+        _ref.map(|(id,name)| format!("{}\t{}", id, name))
+    };
+
+    parse_arg_flag(args, "--refs")?;
+    let url = parse_arg(args, "repository URL")?;
+    let url = [url.as_str(), "/info/refs?service=git-receive-pack"].concat();
+
+    let http = Client::new();
+    let bytes = http.get(&url).send()?.bytes()?;
+
+    let refs = pkt_lines(bytes)
+        .skip(2) // skip command & flush-pkt
+        .take_while(|pkt_line| pkt_line != "")
+        .map(parse_ref)
+        .map(print_ref)
+        .collect::<R<Vec<_>>>()?
+        .join("\n");
+
+    Ok(refs)
+}
+
+fn write_tree() -> R<String> {
+    fn render_entry(entry: &DirEntry) -> R<Vec<u8>> {
+        fn render_file(entry: &DirEntry) -> R<Vec<u8>> {
             let mode_and_name = format!("100644 {}\x00", name(entry));
             let content = create_blob_from_file(&entry.path())?;
             let sha = Sha1::digest(&content.as_bytes());
@@ -138,7 +200,7 @@ fn write_tree() -> Res<String> {
             Ok(row)
         }
 
-        fn render_dir(entry: &DirEntry) -> Res<Vec<u8>> {
+        fn render_dir(entry: &DirEntry) -> R<Vec<u8>> {
             let mode_and_name = format!("040000 {}\x00", name(entry));
             let sha = write_tree(&entry.path())?;
             let mut row = Vec::from(mode_and_name.as_bytes());
@@ -156,7 +218,7 @@ fn write_tree() -> Res<String> {
         }
     }
 
-    fn write_tree(path: &Path) -> Res<[u8; 20]> {
+    fn write_tree(path: &Path) -> R<[u8; 20]> {
         let mut dir_entries = fs::read_dir(path)?.collect::<Result<Vec<_>, _>>()?;
         dir_entries.sort_by_key(name);
         let tree_entries = dir_entries.iter()
@@ -178,11 +240,11 @@ fn write_tree() -> Res<String> {
 
 // helper functions
 
-fn parse_arg(args: &mut Peekable<Args>, info: &str) -> Res<String> {
-    args.next().ok_or(format!("Not enough arguments provided: missing {} argument.", info).into())
+fn parse_arg(args: &mut Peekable<Args>, info: &str) -> R<String> {
+    args.next().ok_or(format!("Not enough arguments provided: missing {}.", info).into())
 }
 
-fn parse_arg_named(args: &mut Peekable<Args>, name: &str) -> Res<String> {
+fn parse_arg_named(args: &mut Peekable<Args>, name: &str) -> R<String> {
     let arg = args.next()
         .ok_or(format!("Not enough arguments provided: missing '{}'.", name))?;
     if arg == name {
@@ -193,7 +255,7 @@ fn parse_arg_named(args: &mut Peekable<Args>, name: &str) -> Res<String> {
     }
 }
 
-fn parse_opt_arg_named(args: &mut Peekable<Args>, name: &str) -> Res<Option<String>> {
+fn parse_opt_arg_named(args: &mut Peekable<Args>, name: &str) -> R<Option<String>> {
     match args.peek() {
         Some(arg) if arg == name => {
             args.next(); // discard peeked arg name
@@ -205,7 +267,7 @@ fn parse_opt_arg_named(args: &mut Peekable<Args>, name: &str) -> Res<Option<Stri
     }
 }
 
-fn parse_arg_flag(args: &mut Peekable<Args>, flag: &str) -> Res<()> {
+fn parse_arg_flag(args: &mut Peekable<Args>, flag: &str) -> R<()> {
     let arg = args.next()
         .ok_or(format!("Not enough arguments provided: missing flag '{}'.", flag))?;
     if arg == flag {
@@ -215,14 +277,14 @@ fn parse_arg_flag(args: &mut Peekable<Args>, flag: &str) -> Res<()> {
     }
 }
 
-fn validate_sha(sha: &str) -> Res<()> {
+fn validate_sha(sha: &str) -> R<()> {
     match sha.len() {
         40 => Ok(()),
         _  => Err("SHA does not have the required length of 40 characters.".into())
     }
 }
 
-fn open_object(sha: &str) -> Res<fs::File> {
+fn open_object(sha: &str) -> R<fs::File> {
     let (dir, filename) = sha.split_at(2);
     let path = ["./.git/objects/", dir, "/", filename].concat();
     let file = fs::File::open(&path)
@@ -230,7 +292,7 @@ fn open_object(sha: &str) -> Res<fs::File> {
     Ok(file)
 }
 
-fn inflate_utf8(file: fs::File) -> Res<String> {
+fn inflate_utf8(file: fs::File) -> R<String> {
     let mut decoder = ZlibDecoder::new(file);
     let mut decompressed = String::new();
     decoder.read_to_string(&mut decompressed)
@@ -238,7 +300,7 @@ fn inflate_utf8(file: fs::File) -> Res<String> {
     Ok(decompressed)
 }
 
-fn inflate_binary(file: fs::File) -> Res<Vec<u8>> {
+fn inflate_binary(file: fs::File) -> R<Vec<u8>> {
     let mut decoder = ZlibDecoder::new(file);
     let mut decompressed = Vec::new();
     decoder.read_to_end(&mut decompressed)
@@ -246,7 +308,7 @@ fn inflate_binary(file: fs::File) -> Res<Vec<u8>> {
     Ok(decompressed)
 }
 
-fn create_blob_from_file(file: &Path) -> Res<String> {
+fn create_blob_from_file(file: &Path) -> R<String> {
     let mut data = String::new();
     fs::File::open(file)?.read_to_string(&mut data)
         .map_err(|e| format!("Failed to read file '{}': {}.", file.to_string_lossy(), e))?;
@@ -261,7 +323,7 @@ fn print_sha(sha: &[u8]) -> String {
         .fold(String::new(), |sha, hex| sha + &hex)
 }
 
-fn create_object(sha: &str) -> Res<fs::File> {
+fn create_object(sha: &str) -> R<fs::File> {
     let (dir, filename) = sha.split_at(2);
     fs::create_dir_all(["./.git/objects/", dir].concat())?;
     let path = ["./.git/objects/", dir, "/", filename].concat();
@@ -270,7 +332,7 @@ fn create_object(sha: &str) -> Res<fs::File> {
     Ok(file)
 }
 
-fn write_object(object: fs::File, content: &[u8]) -> Res<()> {
+fn write_object(object: fs::File, content: &[u8]) -> R<()> {
     let mut encoder = ZlibEncoder::new(object, Compression::default());
     encoder.write(content)?;
     Ok(())
